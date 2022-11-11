73410a4da22072426aad990333eaa61da6f44768
SonuJoseph24 <sonujoseph1034@gmail.com>
2022-11-11 13:40:08 -0500

    docker files added

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
     res.send("<h1>Home Page</h1>")
 })
 
-//http://localhost:3000/profile
+//http://localhost:3000/profile 
 app.post('/profile', (req, res) => {
     console.log(req.body)
     res.json(req.body)
